34ec6a2caad85d0360f92f13f83d9e6ea5e003ca
Darin Jan <darinjan13@gmail.com>
2022-05-05 22:15:56 +0800

    Done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 
-// import HomePage from "./pages/HomePage.jsx";
-import Signin from "./pages/Signin.jsx";
-import Signup from "./pages/Signup.jsx";
-import Trending from './pages/Trending.jsx';
-import NewRelease from './pages/NewRelease.jsx';
-import ContinueWatching from './pages/ContinueWatching.jsx';
+import HomePage from "./Pages/HomePage.jsx";
+import Signin from "./Pages/Signin.jsx";
+import Signup from "./Pages/Signup.jsx";
+import Trending from './Pages/Trending.jsx';
+import NewRelease from './Pages/NewRelease.jsx';
+import ContinueWatching from './Pages/ContinueWatching.jsx';
 
 import ProtectedRoutes from './Components/ProtectedRoutes.jsx'
-import NotFound from "./pages/NotFound.jsx";
+import NotFound from "./Pages/NotFound.jsx";
 import { UserAuthContextProvider } from './context/UserAuthContext.js';
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
   return (
         <UserAuthContextProvider>
           <Routes>
-            <Route exact path="/" element={ <Signin /> } />
+            <Route exact path="/" element={ <HomePage /> } />
             <Route exact path="/signin" element={ <Signin /> } />
             <Route exact path="/signup" element={ <Signup /> } />
             <Route exact path="/trending" element={
